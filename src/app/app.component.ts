731430ab4e21b7b861cbe507afb3ec04c48d0c4d
Simon Prouteau <simonprouteau85@gmail.com>
2020-07-15 19:03:49 +0200

    finish herder and darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'julien-chieze-website';
+
+  public isDarkMode: boolean = true;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.initMode();
+  }
+
+  public initMode() {}
+
+  public changeMode(p_isDarkMode) {
+    this.isDarkMode = p_isDarkMode;
+
+    let switchLabel = document.getElementById('switchMode');
+    let position: DOMRect = switchLabel.getBoundingClientRect();
+
+    if (this.isDarkMode) {
+      let darModeDiv: any = document.getElementsByClassName('dark-bg')[0];
+      darModeDiv.style['z-index'] = '0';
+      darModeDiv.style['top'] = position.top + 'px';
+      darModeDiv.style['left'] = position.left + 'px';
+
+      let whiteModeDiv: any = document.getElementsByClassName('white-bg')[0];
+      whiteModeDiv.style['z-index'] = '-1';
+
+      darModeDiv.children[0].animate(
+        [
+          // keyframes
+          {
+            height: '0vw',
+            width: '0vw',
+          },
+          {
+            height: '250vw',
+            width: '250vw',
+          },
+        ],
+        500
+      );
+
+      //changement main et secondary color
+
+      document.documentElement.style.setProperty(
+        '--main-color',
+        'rgb(255, 255, 255)'
+      );
+      document.documentElement.style.setProperty(
+        '--secondary-color',
+        'rgb(0, 0, 0)'
+      );
+    } else {
+      let darModeDiv: any = document.getElementsByClassName('white-bg')[0];
+      darModeDiv.style['z-index'] = '-1';
+      darModeDiv.style['top'] = position.top + 'px';
+      darModeDiv.style['left'] = position.left + 'px';
+
+      let whiteModeDiv: any = document.getElementsByClassName('white-bg')[0];
+      whiteModeDiv.style['z-index'] = '0';
+      whiteModeDiv.children[0].animate(
+        [
+          // keyframes
+          {
+            height: '0vw',
+            width: '0vw',
+          },
+          {
+            height: '250vw',
+            width: '250vw',
+          },
+        ],
+        500
+      );
+
+      //changement main et secondary color
+      document.documentElement.style.setProperty(
+        '--main-color',
+        'rgb(0, 0, 0)'
+      );
+      document.documentElement.style.setProperty(
+        '--secondary-color',
+        'rgb(255, 255, 255)'
+      );
+    }
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() changeMode = new EventEmitter<boolean>();
   constructor() { }
+
+  public isBgBlack: boolean = true;
 
   ngOnInit(): void {
   }
 
+  public colorBgChange() {
+    this.isBgBlack = !this.isBgBlack;
+    this.changeMode.emit(this.isBgBlack);
+  }
+
+  public getLogoSrc() {
+    if(this.isBgBlack) {
+      return "../../../assets/julien-chieze-logo-white.svg"
+    } else {
+      return "../../../assets/julien-chieze-logo-back.svg"
+
+    }
+  }
+  public getYoutubeSrc() {
+    if(this.isBgBlack) {
+      return "../../../assets/youtube-white.svg"
+    } else {
+      return ".../../../assets/youtube-black.svg"
+
+    }
+  }
+  public getInstaSrc() {
+    if(this.isBgBlack) {
+      return "../../../assets/instagram-white.svg"
+    } else {
+      return ".../../../assets/instagram-blakc.svg"
+
+    }
+  }
+  public getTwitterSrc() {
+    if(this.isBgBlack) {
+      return "../../../assets/twitter-white.svg"
+    } else {
+      return ".../../../assets/twitter-black.svg"
+
+    }
+  }
+  public getFacebookSrc() {
+    if(this.isBgBlack) {
+      return "../../../assets/facebook-white.svg"
+    } else {
+      return ".../../../assets/facebook-black.svg"
+
+    }
+  }
 }
